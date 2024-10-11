d736a30ef9eb9c7e65cc2f5fa927011a1f3cf027
Polvere243 <danielercoli@gmail.com>
2024-10-11 12:02:07 +0200

    added an Id to the span and selected the span and the buttons from the DOM

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 // dichiaro una variabile per il conteggio
 let count = 0;
+
+// recupero gli elementi dal DOM
+
+const value = document.getElementById('value');
+const btn = document.querySelectorAll('.btn');

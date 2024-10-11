2cdb4eb5d56b38472a4c81351962978eda1f97ba
Polvere243 <danielercoli@gmail.com>
2024-10-11 15:33:12 +0200

    almost done

--- app.js
+++ app.js
@@ -8,8 +8,21 @@ const btns = document.querySelectorAll('.btn');
 
 // uso un ciclo sui bottoni 
 
-btns.foreach(btn => {
-    btn.addEventListener('click', () => {
+btns.forEach(btn => {
 
+    // aggancio un ascoltatore di eventi a ogni singolo bottone
+    btn.addEventListener('click', e => {
+
+        // uso l'event target per assicurarmi di ascoltare il bottone giusto
+        const styles = e.currentTarget.classList;
+        // stabilisco le condizioni
+        if (styles.contains('decrease')) {
+            count--;
+        } else if (styles.contains('increase')) {
+            count++;
+        } else {
+            count = 0;
+        }
+        value.innerText = count;
     })
 })

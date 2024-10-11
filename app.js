e9ea3d6b046e3f7c0cf519813fb68d5255bc084c
Polvere243 <danielercoli@gmail.com>
2024-10-11 15:38:00 +0200

    added colors according to the counter value

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ btns.forEach(btn => {
             count++;
         } else {
             count = 0;
+        };
+        // determino un colore diverso a seconda dell'operazione che faccio
+        if (count > 0) {
+            value.style.color = 'red';
+        } else if (count < 0) {
+            value.style.color = 'blue';
+        } else {
+            value.style.color = 'black';
         }
         value.innerText = count;
     })

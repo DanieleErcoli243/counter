a90e8b7ed3e0e1530529508618bf86728092967f
Polvere243 <danielercoli@gmail.com>
2024-10-11 15:11:35 +0200

    used a forEach to loop over the buttons and added an event listener to all of them

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ let count = 0;
 // recupero gli elementi dal DOM
 
 const value = document.getElementById('value');
-const btn = document.querySelectorAll('.btn');
+const btns = document.querySelectorAll('.btn');
+
+// uso un ciclo sui bottoni 
+
+btns.foreach(btn => {
+    btn.addEventListener('click', () => {
+
+    })
+})
